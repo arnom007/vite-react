360cd667943de8ea40a5e4238038c687eb973dfe
arnom007 <66096620+arnom007@users.noreply.github.com>
2026-02-19 11:16:19 -0300

    Add geographical data points for 2nd EIA
    This file contains geographical data points extracted from KML for the 2nd EIA, including references, areas, traffic runways, emergency runways, and safety headings.

---
+++ src/points2EIA.js
@@ -0,0 +1,64 @@
+// Pontos extraídos do KML do 2º EIA
+export const points2EIA = [
+  // REFERÊNCIAS
+  { id: 201, name: 'Saturno', aliases: ['saturno'], coords: [-47.23969, -21.98482], info: 'Lagoa a 06 NM radial 110 VOR PIR' },
+  { id: 202, name: 'Falso Saturno', aliases: ['falso saturno'], coords: [-47.21225, -21.99375], info: 'Referência' },
+  { id: 203, name: 'Vírgula', aliases: ['virgula'], coords: [-47.16970, -22.01461], info: 'Referência' },
+  { id: 204, name: 'Ventre', aliases: ['ventre'], coords: [-47.20980, -22.01470], info: 'Referência' },
+  { id: 205, name: 'Lagoa Branca', aliases: ['lagoa branca'], coords: [-47.02818, -21.89603], info: 'Referência' },
+  { id: 206, name: 'Represa', aliases: ['represa'], coords: [-47.01043, -21.62197], info: 'Referência' },
+  { id: 207, name: 'Usina', aliases: ['usina'], coords: [-46.97491, -22.35694], info: 'Referência' },
+  { id: 208, name: 'Corneta', aliases: ['corneta'], coords: [-47.14994, -22.03876], info: 'Referência' },
+  
+  // ÁREAS (SETORES)
+  { id: 209, name: 'Sagitário 190°', aliases: ['sagitario', 'sagitário 190'], coords: [-47.36688, -21.69164], info: 'Área' },
+  { id: 210, name: 'Libra 230°', aliases: ['libra', 'libra 230'], coords: [-47.10756, -21.63364], info: 'Área' },
+  { id: 211, name: 'Virgem 275°', aliases: ['virgem', 'virgem 275'], coords: [-47.00466, -21.88888], info: 'Área' },
+  { id: 212, name: 'Gêmeos 345°', aliases: ['gemeos', 'gêmeos 345'], coords: [-47.03414, -22.16058], info: 'Área' },
+
+  // PISTAS TRÁFEGO
+  { id: 213, name: 'Fazenda Retiro', aliases: ['fazenda retiro'], coords: [-47.23182, -22.08749], info: 'Pista de Tráfego 2000ft' },
+  { id: 214, name: 'Oriçanga', aliases: ['oriçanga', 'oricanga'], coords: [-46.96927, -22.23244], info: 'Pista de Tráfego 2300ft' },
+  { id: 215, name: 'Jacuba', aliases: ['jacuba'], coords: [-46.96004, -21.91968], info: 'Pista de Tráfego 2400ft' },
+  { id: 216, name: 'Irmãos Ribeiro', aliases: ['irmaos ribeiro', 'espírito santo do pinhal'], coords: [-46.77546, -22.15848], info: 'Pista de Tráfego 3000ft' },
+
+  // PISTAS EMERGÊNCIA
+  { id: 217, name: 'Abengoa', aliases: ['abengoa'], coords: [-47.27668, -21.90043], info: 'Pista de Emergência 2000ft' },
+  { id: 218, name: 'Leme', aliases: ['leme'], coords: [-47.38093, -22.22602], info: 'Pista de Emergência 2100ft' },
+  { id: 219, name: 'Laranjal', aliases: ['laranjal'], coords: [-47.16898, -21.88974], info: 'Pista de Emergência 2100ft' },
+  { id: 220, name: 'Araras', aliases: ['araras'], coords: [-47.35789, -22.33823], info: 'Pista de Emergência 2200ft' },
+  { id: 221, name: 'Fazenda Amália', aliases: ['fazenda amalia'], coords: [-47.37183, -21.44416], info: 'Pista de Emergência 2300ft' },
+  { id: 222, name: 'Ademar Ribeiro', aliases: ['ademar ribeiro', 'srp4'], coords: [-47.47007, -21.64190], info: 'Pista de Emergência 2800ft' },
+
+  // PISTAS TRÁFEGO E EMERGÊNCIA
+  { id: 223, name: 'Caiapó', aliases: ['caiapo', 'caiapó'], coords: [-47.04596, -21.52938], info: 'Tráfego e Emergência 2000ft' },
+  { id: 224, name: 'Tambaú', aliases: ['tambau', 'tambaú'], coords: [-47.31568, -21.69696], info: 'Tráfego e Emergência 2400ft' },
+  { id: 225, name: 'Usina Cresciumal', aliases: ['usina cresciumal', 'cresciumal'], coords: [-47.27831, -22.14834], info: 'Tráfego e Emergência 2000ft' },
+  { id: 226, name: 'Mococa', aliases: ['mococa'], coords: [-47.03369, -21.48715], info: 'Tráfego e Emergência 2100ft' },
+  { id: 227, name: 'Chifre', aliases: ['chifre'], coords: [-47.13058, -22.14923], info: 'Tráfego e Emergência 2200ft' },
+  { id: 228, name: 'Vargem Grande do Sul', aliases: ['vargem grande do sul', 'vargem grande'], coords: [-46.93334, -21.84222], info: 'Tráfego e Emergência 2300ft' },
+  { id: 229, name: 'Casa Branca', aliases: ['casa branca'], coords: [-47.05547, -21.78813], info: 'Tráfego e Emergência 2300ft' },
+  { id: 230, name: 'São João da Boa Vista', aliases: ['sao joao da boa vista', 'são joão'], coords: [-46.84064, -22.01740], info: 'Tráfego e Emergência 2500ft' },
+  { id: 231, name: 'São José do Rio Pardo', aliases: ['sao jose do rio pardo', 'são josé'], coords: [-46.92900, -21.64680], info: 'Tráfego e Emergência 2800ft' },
+
+  // PROAS DE SEGURANÇA (Exemplos extraídos)
+  { id: 232, name: 'Porto Ferreira 155°', aliases: ['porto ferreira', 'porto ferreira 155'], coords: [-47.46197, -21.85035], info: 'Proa de Segurança 155°' },
+  { id: 233, name: 'Santa Rita 180°', aliases: ['santa rita', 'santa rita do passa quatro', 'santa rita 180'], coords: [-47.46590, -21.70617], info: 'Proa de Segurança 180°' },
+  { id: 234, name: 'Santa Rosa 195°', aliases: ['santa rosa', 'santa rosa do viterbo', 'santa rosa 195'], coords: [-47.36708, -21.48931], info: 'Proa de Segurança 195°' },
+  { id: 235, name: 'Tambaú 210°', aliases: ['tambau 210', 'tambaú 210'], coords: [-47.27340, -21.70982], info: 'Proa de Segurança 210°' }
+];
+
+export const AREAS_2EIA = {
+  'Sagitário': ['Sagitário 190°'],
+  'Libra': ['Libra 230°'],
+  'Virgem': ['Virgem 275°'],
+  'Gêmeos': ['Gêmeos 345°']
+};
+
+export const LIMITS_2EIA_GEOJSON = [
+  [[-47.329129,-21.983860], [-46.979278,-22.046485], [-46.757887,-21.979733]], // Limite Virgem/Gêmeos
+  [[-47.250654,-21.823784], [-47.064607,-21.780857], [-46.817456,-21.708874]], // Limite Virgem/Libra
+  [[-47.250168,-21.826646], [-47.273657,-21.706585], [-47.376819,-21.475751]], // Limite Libra/Sagitário
+  [[-47.250147,-21.826615], [-47.239706,-21.984700], [-47.231851,-22.087352]], // Limite Evolução Virgem/Gêmeos
+  [[-47.250163,-21.826341], [-47.119301,-21.948108], [-47.260925,-22.162226]]  // Linha Emergência
+];
